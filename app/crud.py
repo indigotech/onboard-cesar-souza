a2313627f12e708e8eeb39370e026e6b3b28c135
Cesar H. de Souza <cesar.souza@taqtile.com.br>
2025-05-09 16:33:05 -0300

    Update User Model

--- app/crud.py
+++ app/crud.py
@@ -2,8 +2,8 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(name=user.name, email=user.email,password=user.password, birthDate=user.birthDate)
+    db_user = models.User(name=user.name, email=user.email, password=user.password, birthDate=user.birthDate)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return user.name
+    return db_user
