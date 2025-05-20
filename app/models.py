175f433b0cdc8a1557a8d35c5f44a88d2144feb1
cksar <cesarhsouzasoad@gmail.com>
2025-05-20 09:58:01 -0300

    add table address

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
 from .database import Base
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__ = "users"
@@ -9,3 +10,18 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String, index=True)
     birthDate = Column(Date, index=True)
+    addresses = relationship("Address", back_populates="user", cascade="all, delete-orphan")
+
+class Address(Base):
+    __tablename__ = "address"
+
+    id = Column(Integer, primary_key=True, index=True)
+    cep = Column(String)
+    street = Column(String)
+    street_number = Column(String)
+    complement = Column(String)
+    neighborhood = Column(String)
+    city = Column(String)
+    state = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="addresses")
